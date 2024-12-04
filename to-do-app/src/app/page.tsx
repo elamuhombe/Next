5885947c5a9633805ebb084b3208de5b4728eaa5
Elaine Muhombe <emuhombe@gmail.com>
2024-12-04 17:14:00 +0300

    Initialize to-do-app project files

---
+++ to-do-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to ToDo app</h1>
+      <p>This is the main page of the app.</p>
+    </div>
+  );
+}
