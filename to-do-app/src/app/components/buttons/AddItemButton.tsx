988e2a6428b89d391f917d10bfcd2dcba6a5adfd
Elaine Muhombe <emuhombe@gmail.com>
2024-12-06 18:39:04 +0300

    Refactor AddItemButton component: Improve type safety and handle default props

--- to-do-app/src/app/components/buttons/AddItemButton.tsx
+++ to-do-app/src/app/components/buttons/AddItemButton.tsx
@@ -1,19 +1,15 @@
-//src/app/components/buttons/AddItemButton.tsx
-import React from "react";
+// AddItemButton.tsx
+import { ButtonProps } from '@/app/types';
+import React from 'react';
 
-interface AddItemButtonProps {
-  label: string;
-  onClick: () => void;
-}
 
-const AddItemButton: React.FC<AddItemButtonProps> = ({ label, onClick }) => {
+const AddItemButton: React.FC<ButtonProps> = ({ onClick, label="AddItem", className }) => {
   return (
     <button
+    
       onClick={onClick}
-      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
-    >
-      {label}
-    </button>
+      className={`add-item-btn ${className}`} 
+    >{label}</button>
   );
 };
 
