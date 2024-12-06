f9253d6ca5fb8f756ff7bdf292e6a9c6f1e37225
Elaine Muhombe <emuhombe@gmail.com>
2024-12-06 18:49:04 +0300

    feat: Add MarkAsCompletedButton component with default props and type safety

---
+++ to-do-app/src/app/components/buttons/MarkAsCompletedButton.tsx
@@ -0,0 +1,17 @@
+//src/app/components/buttons/MarkAsCompletedButton.tsx
+//src/app/components/buttons/EditButton.tsx
+import { ButtonProps } from '@/app/types';
+import React from 'react';
+
+
+const MarkAsCompletedButton: React.FC<ButtonProps> = ({ onClick, label="MarkAsCompleted", className }) => {
+  return (
+    <button
+    
+      onClick={onClick}
+      className={`add-item-btn ${className}`} 
+    >{label}</button>
+  );
+};
+
+export default MarkAsCompletedButton;
